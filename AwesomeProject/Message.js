f60779e2d913f8494c30fa17fed9af031ed467da
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2024-01-09 20:37:07 +0500

    updated

--- AwesomeProject/Message.js
+++ AwesomeProject/Message.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Text, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Message = () => {
   return (
     <SafeAreaView className="flex-1 flex-col justify-center align-middle ">
-        <View className='text-[#D8B8D1] bg-[#D8B8D1] px-5 m-5 p-3 rounded-lg' >
-             <Text className='text-white text-2xl text-center font-semibold  '>
-               search .....
-            </Text>
-        </View>
+      <View className="text-[#D8B8D1] bg-[#D8B8D1] px-5 m-5 p-3 rounded-lg">
+        <Text className="text-white text-2xl text-center font-semibold  ">
+          <Ionicons name="search" size={22} /> search .....
+        </Text>
+      </View>
       <View className="flex-1 flex-col">
         <View className="flex-1 flex-col justify-between mb-4 px-10 bg-gray-300 p-3 mx-4 rounded-xl">
           <View className="flex-1 flex-row justify-between px-5">
@@ -19,9 +20,7 @@ const Message = () => {
             />
             <View className="mt-2 p-1">
               <Text>Carole Howell $20 /Hr</Text>
-              <Text className="text-blue-600 font-semibold">
-           Just Now
-              </Text>
+              <Text className="text-blue-600 font-semibold">Just Now</Text>
             </View>
           </View>
           <View>
@@ -39,9 +38,7 @@ const Message = () => {
             />
             <View className="mt-2 p-1">
               <Text>Carole Howell $20 /Hr</Text>
-              <Text className="text-blue-600 font-semibold">
-               12 hours
-              </Text>
+              <Text className="text-blue-600 font-semibold">12 hours</Text>
             </View>
           </View>
           <View>
@@ -59,9 +56,7 @@ const Message = () => {
             />
             <View className="mt-2 p-1">
               <Text>Carole Howell $20 /Hr</Text>
-              <Text className="text-blue-600 font-semibold">
-              yesterday
-              </Text>
+              <Text className="text-blue-600 font-semibold">yesterday</Text>
             </View>
           </View>
           <View>
@@ -79,9 +74,7 @@ const Message = () => {
             />
             <View className="mt-2 p-1">
               <Text>Carole Howell $20 /Hr</Text>
-              <Text className="text-blue-600 font-semibold">
-              yesterday
-              </Text>
+              <Text className="text-blue-600 font-semibold">yesterday</Text>
             </View>
           </View>
           <View>
